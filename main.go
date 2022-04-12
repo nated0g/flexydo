b9d5e4e911718202d7a012f9927a17a761203abd
nated0g <n.usher87@gmail.com>
2022-04-11 22:49:38 -0700

    initial datatype definition closes #2

--- main.go
+++ main.go
@@ -8,17 +8,27 @@ import (
 )
 
 type item struct {
+	id       int
 	name     string
 	start    time.Time
 	end      time.Time
 	duration time.Duration
 	fixed    bool
+	active   bool
+	dead     bool
 }
+
+var (
+	past   []item
+	future []item
+)
 
 func newItem(name string, start time.Time, duration time.Duration) *item {
 	i := item{name: name, start: start, duration: duration}
+	i.end = start.Add(duration)
 	return &i
 }
+
 func main() {
 	box := tview.NewBox().
 		SetBorder(true).
